7d891848c94a11688d2dbf84fc27caf30e82b956
Charlie <cschwartz1020@gmail.com>
2019-09-20 22:55:17 -0500

    express backend connected to mongo

--- parents/server.js
+++ parents/server.js
@@ -1,11 +1,61 @@
 const express = require("express");
+var mongo = require("mongodb");
+var url =
+  "mongodb+srv://trash-user:codedownforwhat@cluster0-0p8w9.mongodb.net/test?retryWrites=true&w=majority";
+
+var MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 
-app.get("/api/boilerplate", (req, res) => {
-  const hello = "hello world";
+//GET DOCUMENTS
+app.get("/get-documents", function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("parent-resources");
+    var query = {};
+    dbo
+      .collection("resources")
+      .find(query)
+      .toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
+        db.close();
+      });
+  });
+});
 
-  res.json(hello);
+//GET SURVEYS
+app.get("/get-surveys", function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("parent-resources");
+    var query = {};
+    dbo
+      .collection("surveys")
+      .find(query)
+      .toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
+        db.close();
+      });
+  });
+});
+
+//GET EXTERNAL
+app.get("/get-external-resources", function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("parent-resources");
+    var query = {};
+    dbo
+      .collection("external")
+      .find(query)
+      .toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
+        db.close();
+      });
+  });
 });
 
 const port = 5000;

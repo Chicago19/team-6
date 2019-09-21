4af6a4bad63a4e0b3da773218403472e0c588f66
Ryan Rohm <rjr61@pitt.edu>
2019-09-21 12:02:39 -0400

    fix

--- children6/App.js
+++ children6/App.js
@@ -5,13 +5,11 @@ import {createAppContainer } from 'react-navigation';
 import ScreenOne from './screens/ScreenOne';
 import ScreenTwo from './screens/ScreenTwo';
 import ChildScreen from './screens/ChildScreen';
-import ParentScreen from './screens/ParentScreen';
 
 const App = createStackNavigator({
     ScreenOne: { screen: ScreenOne},
     ScreenTwo: { screen: ScreenTwo},
     ChildScreen: {screen: ChildScreen},
-    ParentScreen: {screen: ParentScreen},
 })
 
 export default createAppContainer(App);

21e26b964a45528ad61ccb110a85503a179e8767
RishiGit <rishikrishnan@hotmail.com>
2019-09-20 20:59:13 -0500

    finally got app background color

--- children6/App.js
+++ children6/App.js
@@ -29,17 +29,17 @@ const FadeInView = (props) => {
 // You can then use your `FadeInView` in place of a `View` in your components:
 export default () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <FadeInView style={{width: 250, height: 50, backgroundColor: 'purple'}}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "black"}}>
+      <FadeInView style={{width: 250, height: 50, backgroundColor: '#865ab0'}}>
         <Button title="Parents" color="white"/>
       </FadeInView>
-      <FadeInView style={{width: 250, height: 50, backgroundColor: 'white'}}>
+      <FadeInView style={{width: 250, height: 50, backgroundColor: 'black'}}>
         <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}></Text>
       </FadeInView>
-      <FadeInView style={{width: 250, height: 50, backgroundColor: 'white'}}>
+      <FadeInView style={{width: 250, height: 50, backgroundColor: 'black'}}>
         <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}></Text>
       </FadeInView>
-      <FadeInView style={{width: 250, height: 50, backgroundColor: 'purple'}}>
+      <FadeInView style={{width: 250, height: 50, backgroundColor: '#865ab0'}}>
         <Button title="Children" color="white" onPress={() => Alert.alert('Go to child page')}/>
       </FadeInView>
     </View>

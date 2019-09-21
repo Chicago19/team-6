2748ee066e4aba5758faef2ed5243eb33e53801a
Ryan Rohm <rjr61@pitt.edu>
2019-09-21 11:11:34 -0400

    fixed typo and sizing

--- children6/screens/ScreenOne.js
+++ children6/screens/ScreenOne.js
@@ -10,7 +10,7 @@ import {
 
 class ScreenOne extends Component {
   static navigationOptions = {
-    title: "Chicage Children's Advocacy Center"
+    title: "Chicago Children's Advocacy Center"
   };
   render() {
     const { navigate } = this.props.navigation;
@@ -20,7 +20,7 @@ class ScreenOne extends Component {
 
       
         <ImageBackground source={require('../static/greenfractal.jpg')} style={{width: '100%', height: '100%'}}>
-        <Image source={require('../static/Childrens-Advocacy-Center.png')} style={{marginTop: "50%", alignSelf: 'center' ,height: '28%', width: '60%'}}/>
+        <Image source={require('../static/Childrens-Advocacy-Center.png')} style={{marginTop: "50%", alignSelf: 'center'}}/>
         <TouchableHighlight
           onPress={() => navigate("ScreenTwo", {screen: "Screen Two"})}
           style={styles.button}>

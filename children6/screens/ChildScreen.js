977ca574cb128895292c32c4e3ae0324d6de9a5d
Charlie <cschwartz1020@gmail.com>
2019-09-21 04:38:15 -0500

    added button with route to app store

--- children6/screens/ChildScreen.js
+++ children6/screens/ChildScreen.js
@@ -1,24 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Linking } from "react-native";
+
 import {
   StyleSheet,
   View,
   Text,
-  TouchableHighlight
-} from 'react-native';
+  TouchableHighlight,
+  Button
+} from "react-native";
 
 class ScreenThree extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePress = this.handlePress.bind(this);
+  }
   static navigationOptions = ({ navigation }) => ({
     title: `Welcome ${navigation.state.params.screen}`
   });
+
+  handlePress() {
+    Linking.openURL("https://apps.apple.com");
+    this.props.onPress && this.props.onPress();
+  }
 
   render() {
     const { params } = this.props.navigation.state;
     return (
       <View style={styles.container}>
         <Text style={styles.titleText}>{params.screen}</Text>
+        <TouchableHighlight style={styles.button} onPress={this.handlePress}>
+          <Text style={styles.buttonText}>Play Games</Text>
+        </TouchableHighlight>
         <TouchableHighlight
           style={styles.button}
-          onPress={() => this.props.navigation.goBack()}>
+          onPress={() => this.props.navigation.goBack()}
+        >
           <Text style={styles.buttonText}>Go Back</Text>
         </TouchableHighlight>
       </View>
@@ -29,27 +46,27 @@ class ScreenThree extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   titleText: {
     fontSize: 22
   },
   button: {
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     marginRight: 25,
     marginLeft: 25,
     marginTop: 20,
     borderRadius: 20,
-    backgroundColor: '#C56EE0',
+    backgroundColor: "#C56EE0",
     height: 50,
-    justifyContent: 'center'
+    justifyContent: "center"
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    alignSelf: 'center'
+    alignSelf: "center"
   }
-})
+});
 
 export default ScreenThree;
--- children6/screens/ScreenOne.js
+++ children6/screens/ScreenOne.js
@@ -1,57 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableHighlight,
   ImageBackground
-} from 'react-native';
+} from "react-native";
 
 class ScreenOne extends Component {
   static navigationOptions = {
     title: "Welcome"
-  }
+  };
   render() {
     const { navigate } = this.props.navigation;
     console.log(this.props, "props is here");
     return (
       <View style={styles.container}>
-         <ImageBackground source={require('../static/purplehex.jpg')} style={{width: '100%', height: '100%'}}>
-        <TouchableHighlight
-          onPress={() => navigate("ScreenTwo", {screen: "Screen Two"})}
-          style={styles.button}>
-       
-        <Text style={styles.buttonText}>Tap To Begin</Text>
-        
-        </TouchableHighlight>
+        <ImageBackground
+          source={require("../static/purplehex.jpg")}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <TouchableHighlight
+            onPress={() => navigate("ScreenTwo", { screen: "Screen Two" })}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>CLICK ME !</Text>
+          </TouchableHighlight>
         </ImageBackground>
-       
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   button: {
-    flex:1,
-    alignSelf: 'stretch',
+    flex: 1,
+    alignSelf: "stretch",
     marginLeft: 10,
     marginRight: 10,
     borderRadius: 5,
     height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center"
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 22,
-    alignSelf: 'center'
+    alignSelf: "center"
   }
 });
 export default ScreenOne;

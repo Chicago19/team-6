51854ac6d333211e6bb3e1ce7758510146b64f2d
RishiGit <rishikrishnan@hotmail.com>
2019-09-20 22:41:26 -0500

    changed home background

--- children6/App.js
+++ children6/App.js
@@ -1,73 +1,78 @@
 import React, { useState, useEffect } from 'react';
 import { Animated, Text, View, Alert, Button, ImageBackground } from 'react-native';
 
-const FadeInView = (props) => {
-  const [fadeAnim] = useState(new Animated.Value(0))  // Initial value for opacity: 0
+  const FadeInView = (props) => {
+    const [fadeAnim] = useState(new Animated.Value(0))  // Initial value for opacity: 0
 
-  React.useEffect(() => {
-    Animated.timing(
-      fadeAnim,
-      {
-        toValue: 1,
-        duration: 1000,
-      }
-    ).start();
-  }, [])
+    React.useEffect(() => {
+      Animated.timing(
+        fadeAnim,
+        {
+          toValue: 1,
+          duration: 1000,
+        }
+      ).start();
+    }, [])
 
-  return (
-    <Animated.View                 // Special animatable View
-      style={{
-        ...props.style,
-        opacity: fadeAnim,         // Bind opacity to animated value
-      }}
-    >
-      {props.children}
-    </Animated.View>
-  );
-}
+    return (
+      <Animated.View                 // Special animatable View
+        style={{
+          ...props.style,
+          opacity: fadeAnim,         // Bind opacity to animated value
+        }}
+      >
+        {props.children}
+      </Animated.View>
+    );
+  }
 
-// You can then use your `FadeInView` in place of a `View` in your components:
-export default () => {
-  return (
-    <ImageBackground source={require('../children6/blackwood.jpg')} style={{width: '100%', height: '100%'}}>
+  // You can then use your `FadeInView` in place of a `View` in your components:
+  export default () => {
+    return (
+      <ImageBackground source={require('../children6/purplehex.jpg')} style={{width: '100%', height: '100%'}}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <FadeInView style={{width: 250, height: 50, backgroundColor: '#865ab0'}}>
-          <Button title="Parents" color="white"/>
-        </FadeInView>
-        <FadeInView style={{width: 250, height: 50}}>
-          <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}></Text>
-        </FadeInView>
-        <FadeInView style={{width: 250, height: 50}}>
-          <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}></Text>
-        </FadeInView>
-        <FadeInView style={{width: 250, height: 50, backgroundColor: '#865ab0'}}>
-          <Button title="Children" color="white" onPress={() => Alert.alert('Go to child page')}/>
-        </FadeInView>
+      <View style={{width: 300, height: 50, backgroundColor: 'white'}}>
+        <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Child Abuse Center</Text>
       </View>
-    </ImageBackground>
+      </View>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <FadeInView style={{width: 250, height: 40, backgroundColor: 'white'}}>
+            <Button title="Parents" color="black"/>
+          </FadeInView>
+          <FadeInView style={{width: 250, height: 50}}>
+            <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}></Text>
+          </FadeInView>
+          <FadeInView style={{width: 250, height: 50}}>
+            <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}></Text>
+          </FadeInView>
+          <FadeInView style={{width: 250, height: 40, backgroundColor: 'white'}}>
+            <Button title="Children" color="black" onPress={() => Alert.alert('Go to child page')}/>
+          </FadeInView>
+        </View>
+      </ImageBackground>
 
 
-  )
+    )
 
-  const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16,
-  },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
+    const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: Constants.statusBarHeight,
+      marginHorizontal: 16,
+    },
+    title: {
+      textAlign: 'center',
+      marginVertical: 8,
+    },
+    fixToText: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    separator: {
+      marginVertical: 8,
+      borderBottomColor: '#737373',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+  });
 
 }

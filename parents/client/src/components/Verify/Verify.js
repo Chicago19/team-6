32a38072a9506be6319380c91ff321c42b2e0f58
anthony <99anthonylam@gmail.com>
2019-09-21 08:52:35 -0400

    Verify css

--- parents/client/src/components/Verify/Verify.js
+++ parents/client/src/components/Verify/Verify.js
@@ -22,15 +22,21 @@ class Verify extends Component {
       document.getElementById("error-msg").innerHTML = "Password incorrect. Please try again."
       }
     }
+
+  componentWillUnmount() {
+    setTimeout(window.history.forward(),0);
+    window.onunload=function(){};
+  }
+
   render() {
     return (
       <div id='verify-container'>
       <p id="pw-text">Enter the password from the receptionist below:</p>
-          <form onSubmit={this.handleSubmit}>
-                  <input type="password" value={this.state.value} onChange={this.handleChange}/>
-              <input type="submit" value="Submit" />
-          </form>
-          <p id="error-msg" />
+      <form onSubmit={this.handleSubmit}>
+              <input type="password" value={this.state.value} onChange={this.handleChange}/>
+          <input type="submit" value="Submit" />
+      </form>
+      <p id="error-msg" />
       </div>
     );
   }
